8574d02ac57128190e5a6df09e882f370b0c9069
Your Name <leoexori@hotmail.com>
2024-03-03 18:49:01 -0300

    feat: add intro links

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,6 +5,10 @@ import Image from "next/image";
 import React from "react";
 import profileImg from "@/public/profile.png";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
@@ -49,9 +53,8 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Olá! Eu sou Leonardo.</span> Eu
-        sou um{" "}
-        <span className="font-bold">desenvolvedor full-stack</span>{" "}
+        <span className="font-bold">Olá! Eu sou Leonardo.</span> Sou
+        um <span className="font-bold">desenvolvedor full-stack</span>{" "}
         com <span className="font-bold">2 anos</span> de experiência.
         Entusiasta de{" "}
         <span className="italic">programação, ciclismo e xadrez</span>
@@ -61,6 +64,41 @@ export default function Intro() {
         </span>
         .
       </motion.p>
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-3 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none hover:scale-105 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contate-me aqui{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-2 transition" />
+        </Link>
+        <a
+          className="group bg-white text-dark px-7 py-3 flex items-center gap-2 rounded-full outline-none hover:scale-105 active:scale-105 transition cursor-pointer border-black/10"
+          href="/CV.pdf"
+          download
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1" />
+        </a>
+        <a
+          className="bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full hover:scale-110 active:scale-110 transition cursor-pointer border-black/10 hover:text-gray-950"
+          href="https://www.linkedin.com/in/leonardolimaalves/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          className="bg-white text-gray-700 p-4 flex text-[1.35rem] items-center gap-2 rounded-full hover:scale-110 active:scale-110 transition cursor-pointer border-black/10 hover:text-gray-950"
+          href="https://github.com/caritas-html"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
